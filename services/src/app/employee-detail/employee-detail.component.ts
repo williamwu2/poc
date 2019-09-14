f84e86797ad2f3c2e20e5cab8c77a741b9648fb4
william wu <williamwu2@hotmail.com>
2019-09-14 13:39:48 -0400

    services

---
+++ services/src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'employee-detail',
+  template: `
+  <h2>Emloyee List</h2>
+  <ul *ngFor="let employee of employees">
+    <li>{{employee.id}} . {{employee.name}} - {{employee.age}}</li>
+  </ul>
+  `,
+  styles: []
+})
+export class EmployeeDetailComponent implements OnInit {
+
+  public employees =[];
+
+  constructor(private _employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.employees = this._employeeService.getEmplyees();
+  }
+
+}
